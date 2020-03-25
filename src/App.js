4127bbc6a838f87daa4a77c6b2687e8f46482c59
Emmanuel Rocha <emmanuelrocha001@gmail.com>
2020-03-25 18:37:16 -0500

    date selection implemented

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 
 import logo from './diet.png'
+import left from './left-arrow.png'
+import right from './right-arrow.png'
 
 function MealContainer(){
   return (
@@ -11,15 +13,118 @@ function MealContainer(){
   );
 }
 
+
+function formatDate(d) {
+  var dMonth = (d.getMonth() + 1).toString().length < 2 ?  '0'+ (d.getMonth()+1).toString() : (d.getMonth()+1).toString()
+  var dDay = (d.getDate()).toString().length < 2 ?  '0'+ (d.getDate()).toString() : (d.getDate()).toString()
+  var dYear = d.getFullYear().toString();
+  var dString = dMonth + '/' + dDay + '/' + dYear;
+  return dString;
+}
+
+function DatePicker(props){
+
+  var today = new Date();
+  var n = today.getTime();
+  n -= 86400000;
+  var yesterday = new Date(n);
+  n += 172800000;
+  var tomorrow = new Date(n)
+  // alert(yesterday);
+  // alert(tomorrow);
+  
+  var todayString = formatDate(today);
+  var yesterdayString = formatDate(yesterday);
+  var tomorrowString = formatDate(tomorrow);
+  var dateString = formatDate(props.currentDate);
+
+  var actualDateString =''
+  if(dateString == todayString) {
+    actualDateString = "TODAY";
+  } else if(dateString == yesterdayString) {
+    actualDateString = "YESTERDAY";
+  } else if(dateString == tomorrowString) {
+    actualDateString = "TOMORROW";
+  } else {
+    actualDateString = dateString;
+  }
+  
+  return (
+    <div className="DateContainer">
+      
+      <div className="CurrentDate">
+        <img className="ArrowButton" onClick={props.handleDateDecrement} src={left}>
+        </img>
+      </div>
+      <div className="CurrentDate">{actualDateString}</div>
+      <div className="CurrentDate">
+        <img className="ArrowButton" src={right} onClick={props.handleDateIncrement}>
+        </img>
+      </div>
+
+      <input onChange={props.handleDateChange} className="Calendar" type="date"/>
+  </div>  
+  );
+}
+
 function Greeting(props){
   return (
     <p className="Greeting">Hello, {props.userName}</p>
   );
 }
+
+
+
+
 class App extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      selectedDate: new Date()
+    }
+
+
+    this.handleDateChange = this.handleDateChange.bind(this);
+    this.handleDateDecrement = this.handleDateDecrement.bind(this);
+    this.handleDateIncrement = this.handleDateIncrement.bind(this);
+
   }
+
+
+  handleDateIncrement() {
+    var n = this.state.selectedDate.getTime();
+    n += 86400000;
+    var newDate = new Date(n);
+
+    this.setState({
+      selectedDate: newDate
+    });
+
+  }
+
+  handleDateDecrement() {
+    var n = this.state.selectedDate.getTime();
+    n -= 86400000;
+    var newDate = new Date(n);
+
+    this.setState({
+      selectedDate: newDate
+    });
+
+  }
+  handleDateChange(event) {
+    var eventYear = event.target.value.substring(0,4);
+    var eventMonth = event.target.value.substring(5,7);
+    var eventDay = event.target.value.substring(8,10);
+    var eventDate = eventMonth + '/' + eventDay + '/'+eventYear;
+
+    var dateSelected = new Date(eventDate);
+    this.setState({
+      selectedDate: dateSelected
+    });
+  }
+
   render() {
 
 
@@ -29,11 +134,14 @@ class App extends React.Component {
         <h1>FoodTracker</h1>
         <img className="Logo" src={logo}></img>
       </div>
-      <Greeting userName="Emmanuel"/>
+
+      <DatePicker currentDate={this.state.selectedDate} handleDateChange={this.handleDateChange} handleDateIncrement={this.handleDateIncrement} handleDateDecrement={this.handleDateDecrement}/>
+      <div className="Body">
       <MealContainer />
       <MealContainer />
       <MealContainer />
       <MealContainer />
+      </div>
 
 
     </div>
