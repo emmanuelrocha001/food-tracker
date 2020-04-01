75f931d30abd05e31b4c497e2c4e27231cb006fa
Emmanuel Rocha <emmanuelrocha001@gmail.com>
2020-03-31 20:57:09 -0500

    Updates

---
+++ precache-manifest.a256dcd82f4853e102e494249808a2c6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fea141a6d19344c07cb0fd8204ea379b",
+    "url": "/food-tracker/index.html"
+  },
+  {
+    "revision": "f3d69b1ca02a2110acc0",
+    "url": "/food-tracker/static/css/main.dc44c036.chunk.css"
+  },
+  {
+    "revision": "5d996ea06acadf5a4af5",
+    "url": "/food-tracker/static/js/2.64ce5cbc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/food-tracker/static/js/2.64ce5cbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3d69b1ca02a2110acc0",
+    "url": "/food-tracker/static/js/main.e5f1a62d.chunk.js"
+  },
+  {
+    "revision": "481b91c8d0c811d9cc4b",
+    "url": "/food-tracker/static/js/runtime-main.76013246.js"
+  },
+  {
+    "revision": "7af79a95d4ac842032de75eaaa55311d",
+    "url": "/food-tracker/static/media/cross.7af79a95.png"
+  },
+  {
+    "revision": "cf35e2f9e810e53c10d058841598d7d3",
+    "url": "/food-tracker/static/media/diet.cf35e2f9.png"
+  },
+  {
+    "revision": "3db8b548181ee54ae69969700ee102fd",
+    "url": "/food-tracker/static/media/left-arrow.3db8b548.png"
+  },
+  {
+    "revision": "fbd996f1e26a7c9357643e3dfd7af0ba",
+    "url": "/food-tracker/static/media/plus.fbd996f1.png"
+  },
+  {
+    "revision": "30cacd844392a00bcb43417d619f658d",
+    "url": "/food-tracker/static/media/right-arrow.30cacd84.png"
+  }
+]);
